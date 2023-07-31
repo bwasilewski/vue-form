33ee6d20ca2f984d955bdc6e4332aa711557de05
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-07-31 11:43:52 -0500

    create a form component and pass in an array of fields

---
+++ src/components/AppForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <fieldset v-for="field in fields" :key="field.name">
+      <label :for="field.name">{{ field.name }}</label>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AppForm",
+  props: {
+    fields: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AppForm :fields="fields" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AppForm from "@/components/AppForm.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    AppForm,
+  },
+  data() {
+    return {
+      fields: [
+        {
+          name: "full-name",
+          label: "Full Name",
+          type: "text",
+          rules: [
+            {
+              required: true,
+              message: "Please enter your full name",
+            },
+            {
+              min: 3,
+              message: "Your name must be at least 3 characters long",
+            },
+          ],
+        },
+        {
+          name: "phone",
+          label: "Telephone Number",
+          type: "tel",
+          rules: [
+            {
+              numeric: true,
+            },
+          ],
+        },
+        {
+          name: "interest",
+          label: "Interest",
+          type: "select",
+          options: [
+            { label: "Frontend", value: "frontend" },
+            { label: "Backend", value: "backend" },
+            { label: "DevOps", value: "devops" },
+            { label: "QA", value: "qa" },
+          ],
+          rules: [
+            {
+              required: true,
+              message: "Please select your interest",
+            },
+          ],
+        },
+        {
+          name: "description",
+          label: "Description",
+          type: "textarea",
+          rules: [
+            {
+              max: 500,
+            },
+          ],
+        },
+        {
+          name: "reference",
+          label: "Reference",
+          type: "checkboxgroup",
+          options: [
+            { label: "Online Ad", value: "online-ad" },
+            { label: "Recommendation", value: "recommendation" },
+            { label: "Referral", value: "referral" },
+            { label: "Other", value: "other" },
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
