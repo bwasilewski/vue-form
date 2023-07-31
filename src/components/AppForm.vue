83ae3d8b524464a4efa076d82ceb564583ccc7af
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-07-31 12:07:22 -0500

    create a select field component

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -10,17 +10,26 @@
         :required="field.rules.some((rule) => rule.required)"
         :placeholder="field.placeholder"
       />
+
+      <SelectField
+        v-if="field.type === 'select'"
+        :value="field.value"
+        :required="field.rules.some((rule) => rule.required)"
+        :options="field.options"
+      />
     </fieldset>
   </form>
 </template>
 
 <script>
 import TextField from "@/components/fields/TextField.vue";
+import SelectField from "@/components/fields/SelectField.vue";
 
 export default {
   name: "AppForm",
   components: {
     TextField,
+    SelectField,
   },
   props: {
     fields: {
---
+++ src/components/fields/SelectField.vue
@@ -0,0 +1,28 @@
+<template>
+  <select :value="value" :required="required">
+    <option disabled value="">Please select one</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.label }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "SelectField",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
