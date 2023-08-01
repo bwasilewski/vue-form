9bc6f919f3a2f011caac094b004c6e6c2568b546
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-08-01 11:41:51 -0500

    validate field and show validation status

--- src/components/fields/SelectField.vue
+++ src/components/fields/SelectField.vue
@@ -1,16 +1,39 @@
 <template>
-  <select :value="value" :required="required">
+  <select
+    v-if="isMounted"
+    :value="value"
+    :required="required"
+    :name="name"
+    :ref="name"
+  >
     <option disabled value="">Please select one</option>
     <option v-for="option in options" :key="option.value" :value="option.value">
       {{ option.label }}
     </option>
   </select>
+  <p v-if="!validate()">Invalid</p>
 </template>
 
 <script>
 export default {
   name: "SelectField",
+  data() {
+    return {
+      isMounted: false,
+    };
+  },
+  mounted() {
+    this.isMounted = true;
+  },
   props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
     value: {
       type: String,
       default: "",
@@ -22,6 +45,31 @@ export default {
     options: {
       type: Array,
       required: true,
+    },
+  },
+  computed: {
+    formIsValid() {
+      return this.validate();
+    },
+  },
+  methods: {
+    validate() {
+      if (!this.isMounted || !this.$refs[this.name]) {
+        return true;
+      }
+
+      const value = this.$refs[this.name].value;
+      let isValid = true;
+
+      this.rules.forEach((rule) => {
+        if (rule.required === true) {
+          if (value === "") {
+            isValid = false;
+          }
+        }
+      });
+
+      return isValid;
     },
   },
 };
--- src/components/fields/TextField.vue
+++ src/components/fields/TextField.vue
@@ -1,10 +1,26 @@
 <template>
-  <input :type="type" :value="value" :name="name" />
+  <input
+    v-if="isMounted"
+    :type="type"
+    :value="value"
+    :name="name"
+    :ref="name"
+  />
+  <p v-if="isFormSubmitted && !formIsValid">Invalid</p>
 </template>
 
 <script>
 export default {
   name: "TextField",
+  data() {
+    return {
+      isMounted: false,
+    };
+  },
+  mounted() {
+    this.isMounted = true;
+    console.log("Is Form Submitted, text field: ", this.isFormSubmitted);
+  },
   props: {
     name: {
       type: String,
@@ -21,6 +37,49 @@ export default {
     required: {
       type: Boolean,
       default: false,
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
+    isFormSubmitted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    formIsValid() {
+      return this.validate();
+    },
+  },
+  methods: {
+    validate() {
+      if (!this.isFormSubmitted || !this.isMounted || !this.$refs[this.name]) {
+        return true;
+      }
+
+      const value = this.$refs[this.name].value;
+      let isValid = true;
+
+      this.rules.forEach((rule) => {
+        if (rule.required === true) {
+          if (value.length === 0) {
+            isValid = false;
+          }
+        }
+        if (rule.min) {
+          if (value.length < rule.min) {
+            isValid = false;
+          }
+        }
+        if (rule.numeric) {
+          if (isNaN(parseInt(value))) {
+            isValid = false;
+          }
+        }
+      });
+
+      return isValid;
     },
   },
 };
