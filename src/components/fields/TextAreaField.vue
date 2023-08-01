7bbdddc0d7eb1ec8b5ede381d62bca72607006fc
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-08-01 16:02:29 -0500

    handle textarea field

--- src/components/fields/TextAreaField.vue
+++ src/components/fields/TextAreaField.vue
@@ -1,11 +1,31 @@
 <template>
-  <textarea :value="value" :required="required"></textarea>
+  <p v-if="isFormSubmitted && !fieldIsValid">Invalid</p>
+  <textarea
+    v-if="isMounted"
+    v-model="fieldValue"
+    :required="required"
+    :name="name"
+  >
+  </textarea>
 </template>
 
 <script>
 export default {
   name: "TextField",
+  data() {
+    return {
+      isMounted: false,
+      fieldValue: this.value,
+    };
+  },
+  mounted() {
+    this.isMounted = true;
+  },
   props: {
+    name: {
+      type: String,
+      required: true,
+    },
     value: {
       type: String,
       required: true,
@@ -13,6 +33,52 @@ export default {
     required: {
       type: Boolean,
       default: false,
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
+    isFormSubmitted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    fieldIsValid() {
+      return this.validate();
+    },
+    computedValue: {
+      get() {
+        return this.fieldValue;
+      },
+      set(newValue) {
+        this.fieldValue = newValue;
+      },
+    },
+  },
+  methods: {
+    validate() {
+      if (!this.isFormSubmitted || !this.isMounted) {
+        return true;
+      }
+
+      const value = this.fieldValue;
+      let isValid = true;
+
+      this.rules.forEach((rule) => {
+        if (rule.required === true) {
+          if (value.length === 0) {
+            isValid = false;
+          }
+        }
+        if (rule.max) {
+          if (value.length > rule.max) {
+            isValid = false;
+          }
+        }
+      });
+
+      return isValid;
     },
   },
 };
