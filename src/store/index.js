b5669516fc681a74d61b22a629834da22024483a
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-07-31 21:41:20 -0500

    move field data to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,82 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    formData: {
-      fullname: "John Doe",
-      phone: "1234567890",
-      interest: "frontend",
-      description: "Lorem ipsum dolor sit amet",
-    },
+    fields: [
+      {
+        name: "fullname",
+        label: "Full Name",
+        type: "text",
+        rules: [
+          {
+            required: true,
+            message: "Please enter your full name",
+          },
+          {
+            min: 3,
+            message: "Your name must be at least 3 characters long",
+          },
+        ],
+        value: "",
+        placeholder: "Name here",
+      },
+      {
+        name: "phone",
+        label: "Telephone Number",
+        type: "tel",
+        value: "",
+        rules: [
+          {
+            numeric: true,
+            message: "Your phone number must be numeric",
+          },
+        ],
+      },
+      {
+        name: "interest",
+        label: "Interest",
+        type: "select",
+        value: "frontend",
+        options: [
+          { label: "Frontend", value: "frontend" },
+          { label: "Backend", value: "backend" },
+          { label: "DevOps", value: "devops" },
+          { label: "QA", value: "qa" },
+        ],
+        rules: [
+          {
+            required: true,
+            message: "Please select your interest",
+          },
+        ],
+      },
+      {
+        name: "description",
+        label: "Description",
+        type: "textarea",
+        value: "Lorem ipsum dolor sit amet",
+        rules: [
+          {
+            max: 500,
+            message: "Your description must be no longer than 500 characters",
+          },
+        ],
+      },
+      {
+        name: "reference",
+        label: "Reference",
+        type: "checkboxgroup",
+        options: ["Online Ad", "Recommendation", "Referral", "Other"],
+        value: ["Online Ad", "Referral"],
+        rules: [
+          {
+            required: true,
+            message: "Please choose at least one reference",
+          },
+        ],
+      },
+    ],
   },
   getters: {},
   mutations: {},
-  actions: {},
   modules: {},
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <AppForm :fields="fields" />
+    <AppForm :fields="$store.state.fields" />
   </div>
 </template>
 
@@ -11,77 +11,6 @@ export default {
   name: "HomeView",
   components: {
     AppForm,
-  },
-  data() {
-    return {
-      fields: [
-        {
-          name: "fullname",
-          label: "Full Name",
-          type: "text",
-          rules: [
-            {
-              required: true,
-              message: "Please enter your full name",
-            },
-            {
-              min: 3,
-              message: "Your name must be at least 3 characters long",
-            },
-          ],
-          value: "",
-          placeholder: "Name here",
-        },
-        {
-          name: "phone",
-          label: "Telephone Number",
-          type: "tel",
-          rules: [
-            {
-              numeric: true,
-            },
-          ],
-        },
-        {
-          name: "interest",
-          label: "Interest",
-          type: "select",
-          options: [
-            { label: "Frontend", value: "frontend" },
-            { label: "Backend", value: "backend" },
-            { label: "DevOps", value: "devops" },
-            { label: "QA", value: "qa" },
-          ],
-          rules: [
-            {
-              required: true,
-              message: "Please select your interest",
-            },
-          ],
-        },
-        {
-          name: "description",
-          label: "Description",
-          type: "textarea",
-          rules: [
-            {
-              max: 500,
-            },
-          ],
-        },
-        {
-          name: "reference",
-          label: "Reference",
-          type: "checkboxgroup",
-          options: [
-            { label: "Online Ad", value: "online-ad" },
-            { label: "Recommendation", value: "recommendation" },
-            { label: "Referral", value: "referral" },
-            { label: "Other", value: "other" },
-          ],
-        },
-      ],
-    };
   },
 };
 </script>
