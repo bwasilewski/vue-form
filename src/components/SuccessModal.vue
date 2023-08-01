9fa736cf20f4a3a61f445f047cd46d94139f1ca0
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-08-01 16:00:10 -0500

    improve success modal styles

--- src/components/SuccessModal.vue
+++ src/components/SuccessModal.vue
@@ -2,13 +2,20 @@
   <div class="modal" v-if="showSuccessModal">
     <div class="modal-backdrop"></div>
     <div class="modal-content">
-      <h1>This is my modal</h1>
-      <ul>
-        <li v-for="field in fields" :key="field.name">
-          <strong>{{ field.name }}:</strong> {{ field.value }}
-        </li>
-      </ul>
-      <button @click="$store.commit('toggleSuccessModal')">Close</button>
+      <h1>Form submitted successfully! 👍</h1>
+      <table>
+        <tbody>
+          <tr v-for="field in fields" :key="field.name">
+            <td>
+              <strong>{{ field.label }}</strong>
+            </td>
+            <td>{{ field.value }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button class="button" @click="$store.commit('toggleSuccessModal')">
+        Close
+      </button>
     </div>
   </div>
 </template>
@@ -31,16 +38,15 @@ export default {
   top: 0;
   width: 100vw;
   height: 100vh;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.75);
 }
 
 .modal-content {
   position: absolute;
   top: 100px;
-  left: calc((100vw - 400px) / 2);
-  width: 400px;
-  height: 200px;
+  left: calc((100vw - 500px - 6rem) / 2);
+  width: 500px;
   background: #fff;
-  padding: 1rem;
+  padding: 3rem;
 }
 </style>
